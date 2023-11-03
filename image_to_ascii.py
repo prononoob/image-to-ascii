1319b759f5fc6ecac10e9828352b87ff6922678e
Kacper Duda <lollel60606@gmail.com>
2023-11-03 11:11:31 +0100

    Created very early skeleton classes

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -2,39 +2,14 @@ from cv2 import imread
 
 
 class Calculate:
-    '''def width(arrayWidth: int, imageWidth: int) -> int:
-        return int(imageWidth / arrayWidth)
-    
-    def height(arrayHeight: int, imageHeight: int) -> int:
-        return int(imageHeight / arrayHeight)'''
-    
-    def size(imageWidth: int, imageHeight: int) -> list[int]:
-        if imageWidth == imageHeight:
-            return []
-        temp = [imageWidth, imageHeight]
-        # ^ o co tu chodzilo ?
-
-        if imageWidth > imageHeight:
-            pass
+    # Calculate how many pixels to put in one character
+    pass
 
 
 
 class Shader:
     def __init__(self, imagePath: str):
         self.image = imread(imagePath)
-        # ^ type numpy.NDArray
-        # na razie 80x80 znakow probujemy
-        # self.createEmptyResult(arrayWidth, arrayHeight)
-        # int: charWidth, charHeight
-
-        # inny koncept wip
-        self.temp = Calculate.size()
-        self.charWidth = Calculate.widthPixelsToChar(arrayWidth, len(self.image[0]))
-        self.charHeight = Calculate.heightPixelsToChar(arrayHeight, len(self.image))
-        # jakos tak ^ ?? 
-
-        # NAPRAWIC TO ^^^
-
     
     def createEmptyResult(self, arrayWidth, arrayHeight):
         self.result = [[] for x in range(arrayHeight)]
@@ -45,10 +20,7 @@ class Shader:
 
 def main():
     s = Shader('C:/Users/lolle/Desktop/Whiskas.png')
-    #print(s.result[0])
-    '''for i in s.result:
-        print(i)'''
-    print(s.charHeight, s.charWidth)
+    s.createEmptyResult(100, 100)
 
 
 if __name__ == '__main__':
