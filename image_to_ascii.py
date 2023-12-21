66625ad15739c95377094329032957eacec9a8bb
Kacper Duda <lollel60606@gmail.com>
2023-12-21 11:49:53 +0100

    rewritten skeleton class so its static

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -3,24 +3,35 @@ from cv2 import imread
 
 class Calculate:
     # Calculate how many pixels to put in one character
+    # Max length of one line is 100 characters
+    '''@staticmethod
+    def ratioTo ???
+    ratio = 100/x'''
     pass
 
 
 
 class Shader:
-    def __init__(self, imagePath: str):
-        self.image = imread(imagePath)
-    
-    def createEmptyResult(self, arrayWidth: int, arrayHeight: int):
-        self.result = [[] for x in range(arrayHeight)]
-        for i in range(len(self.result)):
+    @staticmethod
+    def createEmptyResult(arrayWidth: int, arrayHeight: int) -> None:
+        result = [[] for x in range(arrayHeight)]
+        for i in range(len(result)):
             for j in range(arrayWidth):
-                self.result[i].append('')
+                result[i].append('a')
+            print(''.join(result[i]))
+    
+    @staticmethod
+    def convert(imagePath: str, customHeight: int = 100) -> None:
+        image = imread(imagePath)
+        Shader.createEmptyResult(customHeight, customHeight)
+    
+    @staticmethod
+    def setCustomHeight(height: int) -> None:
+        customHeight = height
 
 
 def main():
-    s = Shader('C:/Users/lolle/Desktop/Whiskas.png')
-    s.createEmptyResult(100, 100)
+    Shader.convert('C:/Users/lolle/Desktop/Whiskas.png')
 
 
 
