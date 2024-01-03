491099ad76b9f18bc2083e8c0a3c93581d1c34f0
Kacper Duda <lollel60606@gmail.com>
2024-01-03 12:43:08 +0100

    added char array in order of density in greyscale

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -25,20 +25,28 @@ class Shader:
     def convert(imagePath: str, customWidth: int = 100) -> None:
         image = cv2.imread(imagePath)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # for i in range(255):
+            # image[i] = i - i%16
 
         # DEBUG print
         # cv2.imshow('image', image)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
         
+        # chars in order of brightness
+        # chars: `_>csJiIS6VUm$N@
+        # 0 is black, 255 is white
+        chars = ['`', '_', '>', 'c', 's', 'J', 'i', 'I', 'S', '6', 'V', 'U', 'm', '$', 'N', '@']
+        
+
         ratio = Calculate.pixelRatio(customWidth, len(image[0]))
         # print(int(ratio * len(image)))
         print(customWidth, int(len(image) * ratio))
         Shader.createEmptyResult(customWidth, int(len(image) * ratio))
     
     @staticmethod
-    def setcustomWidth(wihth: int) -> None:
-        customWidth = wihth
+    def setcustomWidth(width: int) -> None:
+        customWidth = width
 
 
 def main():
